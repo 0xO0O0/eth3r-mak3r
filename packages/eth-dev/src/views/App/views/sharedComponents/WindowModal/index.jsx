762707249db3c26e4a7fd04ecc56ee83c4e136d9
⚡️0xO0O0⚡️ <creativeKoder@users.noreply.github.com>
2021-06-19 23:08:54 -0400

    Initialize eth-dev

---
+++ packages/eth-dev/src/views/App/views/sharedComponents/WindowModal/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import ReactModal from 'react-modal-resizable-draggable'
+import $ from 'jquery'
+import './styles.css'
+
+export default function WindowModal({
+  uniqueWindowId,
+  backgroundPath,
+  initWidth,
+  initHeight,
+  initTop,
+  initLeft,
+  dragAreaHeightPercent,
+  onFocus,
+  onRequestClose,
+  isOpen,
+  containerStyle,
+  contentContainerStyle,
+  children
+}) {
+  return (
+    <ReactModal
+      className={uniqueWindowId}
+      initWidth={initWidth}
+      initHeight={initHeight}
+      onFocus={onFocus}
+      onRequestClose={onRequestClose}
+      isOpen={isOpen}
+      top={initTop}
+      left={initLeft}
+    >
+      <div
+        className='background-image'
+        style={{
+          height: '100%',
+          overflowY: 'scroll',
+          background: `url(${backgroundPath})`,
+          backgroundSize: '100% 100%'
+        }}
+      />
+
+      <div
+        className='flexible-modal-drag-area'
+        style={{
+          position: 'absolute',
+          right: 0,
+          top: 0,
+          height: dragAreaHeightPercent ? `${dragAreaHeightPercent}%` : '5%',
+          width: '100%',
+          cursor: 'move'
+        }}
+      />
+
+      <div
+        className='content'
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          height: '100%',
+          width: '100%',
+          overflow: 'scroll',
+          ...contentContainerStyle
+        }}
+      >
+        {children}
+      </div>
+    </ReactModal>
+  )
+}
